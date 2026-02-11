22f1099deb3ee212cd7856a81099d31e8ebedae4
sadaqk hassan <sadaqsamor99@gmail.com>
2026-02-11 14:51:08 +0300

    usermodel

---
+++ server/Models/user.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    role:{
+        type:String,
+        enum:["user","admin"]
+    }
+},{timestamps:true});
+
+
+export const userModel = mongoose.model("user",userSchema);
