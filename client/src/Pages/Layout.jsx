2a08896cc0a3884d0db19f878f05ed1df9cc202a
sadaqk hassan <sadaqsamor99@gmail.com>
2026-02-13 11:40:27 +0300

    some padinATON

--- client/src/Pages/Layout.jsx
+++ client/src/Pages/Layout.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Sidebar from '../components/Sidebar'
 
 const Layout = () => {
   return (
-    <div>Layout</div>
+    <div className='flex gap-4 w-full bg-gray-100'>
+      <Sidebar/>
+      <div className='pt-4 pl-4'>
+        <Outlet/>
+      </div>
+    </div>
   )
 }
 
