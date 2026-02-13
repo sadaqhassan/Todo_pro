3eada888eada4289755cd76ea118465ceb264c58
sadaqk hassan <sadaqsamor99@gmail.com>
2026-02-13 11:39:45 +0300

    somefix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,21 @@ import { Route, Routes } from 'react-router-dom'
 import Auth from './Pages/Auth'
 import { UseAuthContext } from './Context/AuthContext'
 import Layout from './Pages/Layout'
+import Dashboard from './Pages/Dashboard'
+import Students from './Pages/Students'
+import Finance from './Pages/Finance'
+import Teachers from './Pages/Teachers'
 
 const App = () => {
   const {user} = UseAuthContext()
   return (
-    <div className='bg-green-500'>
+    <div >
       <Routes>
-        <Route path='/' element={!user ? <Auth/> : <Layout/>}>
+        <Route path='/' element={user ? <Auth/> : <Layout/>}>
+        <Route index element={<Dashboard/>}/>
+        <Route path='students' element={<Students/>}/> 
+        <Route path='teachers' element={<Teachers/>}/> 
+        <Route path='finance' element={<Finance/>}/> 
         </Route>
       </Routes>
     </div>
